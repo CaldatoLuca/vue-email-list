b201c4014ce5b5006bacf5d4bedaa3872e5cd913
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-05 16:18:30 +0100

    feat: visualizzazione di più email

--- js/main.js
+++ js/main.js
@@ -7,11 +7,15 @@ const vueConfig = {
     return {
       apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
       email: null,
+      email2: null,
     };
   },
   created() {
     axios.get(this.apiUrl).then((response) => {
       this.email = response.data.response;
+    });
+    axios.get(this.apiUrl).then((response) => {
+      this.email2 = response.data.response;
     });
   },
 };
